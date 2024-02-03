726aa1c6dda76e0d6e4aa722d846a74f36bbd60c
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-02-02 20:14:50 -0800

    STYLE  header social

--- pages/index.js
+++ pages/index.js
@@ -19,87 +19,99 @@ export default function Home() {
 
 {/* HEADER */}
     <main className={`${styles.main} ${inter.className}`}>
-		<div className={styles.header_grid}>
-	  	<a href="#">Home</a>
-	  	<a href="https://vimeo.com/williamrichardhurst">Reel</a>
-	  	<a href="#">Resume</a>
-	  	<a href="#">Contact</a>
-			<a href="#">IMDB</a>
-	  	<a href="#">Twitter</a>
-		</div>
-		<div className={styles.header_socials}>
-			<a href="#"></a>
-			<a href="#"></a>
-			<a href="#"></a>
-		</div>
+
+			<div className={styles.header_grid}>
+	  		<a href="#">Home</a>
+	  		<a href="https://vimeo.com/williamrichardhurst">Reel</a>
+	  		<a href="#">Resume</a>
+	  		<a href="#">Contact</a>
+			</div>
+
+	<div className={styles.header_socials}>
+			<a 
+			href="https://twitter.com/darkeyesdesign">
+				<img src="/assets/socials/twitter-old-logo.png"
+			 	className={styles.header_socials_image}
+				/>
+			</a>
+			<a href="https://www.youtube.com/@william_r_hurst">
+				<img src="/assets/socials/youtube-logo.png"
+			 	className={styles.header_socials_image}
+				/>
+			</a>
+			<a href="https://pro.imdb.com/name/nm12293482/?ref_=instant_nm_2&q=william%20hurst">
+				<img src="/assets/socials/imdb-logo.png"
+			 	className={styles.header_socials_image}
+				/>
+			</a>
+	</div>
 
 {/* CONTENT */}
-   	<div className={styles.description}>
-    	<p>
-				<img src="/assets/set-shot-122431.jpeg"
-				className={styles.header_image}
-				/>
-			</p>
-    </div>
+   		<div className={styles.description}>
+    		<p>
+					<img src="/assets/set-shot-122431.jpeg"
+					className={styles.header_image}
+					/>
+				</p>
+    	</div>
 
 {/* CENTER IMAGE */}
 		{/*<div className={styles.center}>*/}
-		<div className={styles.content_grid}>
-			<Bio />
-		</div>
+			<div className={styles.content_grid}>
+				<Bio />
+			</div>
 
 {/* STYLES GRID - LATERAL */}
-    <div className={styles.grid}>
-    	<a 
-			href={dreadloreWebsite}
-      className={styles.card}
-      target="_blank"
-      rel="noopener noreferrer"
-      >
-      	<h2>YouTube <span>-&gt;</span>
-					</h2>
-      	<p>Projects
-      		</p>
-      </a>
+    	<div className={styles.grid}>
+    		<a 
+				href={dreadloreWebsite}
+      	className={styles.card}
+      	target="_blank"
+      	rel="noopener noreferrer"
+      	>
+      		<h2>YouTube <span>-&gt;</span>
+						</h2>
+      		<p>Projects
+      			</p>
+      	</a>
 
-      <a
-     	href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      className={styles.card}
-      target="_blank"
-      rel="noopener noreferrer"
-      >
-      	<h2>Learn <span>-&gt;</span>
-	        </h2>
-        <p>Learn about Next.js in an interactive course with&nbsp;quizzes!
-  				</p>
-      </a>
+      	<a
+     		href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      	className={styles.card}
+      	target="_blank"
+      	rel="noopener noreferrer"
+     	 >
+      		<h2>Learn <span>-&gt;</span>
+	      	  </h2>
+      		<p>Learn about Next.js in an interactive course with&nbsp;quizzes!
+  					</p>
+				</a>
 
-      <a
-      href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      className={styles.card}
-      target="_blank"
-      rel="noopener noreferrer"
-      >
-        <h2>Templates <span>-&gt;</span>
-          </h2>
-        <p>Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-      </a>
+      	<a
+      	href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      	className={styles.card}
+      	target="_blank"
+      	rel="noopener noreferrer"
+     	 	>
+        	<h2>Templates <span>-&gt;</span>
+          	</h2>
+        	<p>Discover and deploy boilerplate example Next.js&nbsp;projects.
+          	</p>
+     		</a>
 
-      <a
-      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      className={styles.card}
-      target="_blank"
-      rel="noopener noreferrer"
-      >
-      	<h2>
-        Deploy <span>-&gt;</span>
-        </h2>
-        <p>Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.
-          </p>
-      </a>
-    </div>
-  </main>
+      	<a
+      	href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      	className={styles.card}
+      	target="_blank"
+      	rel="noopener noreferrer"
+      	>
+      		<h2>Deploy <span>-&gt;</span>
+        		</h2>
+        		<p>Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.
+          		</p>
+      	</a>
+    	</div>
+  	</main>
   </>
   )
 }
