ce750d3955f008df88ea2c63782e87b0ae6cc64c
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-02-09 03:52:26 -0800

    finish headshot styling

--- pages/_headshots.js
+++ pages/_headshots.js
@@ -5,22 +5,22 @@ export default function Headshots() {
 	<>
 		<div className={styles.headshots}>
 
-    	<img className={styles.headshots_items}
+    	<img className={[styles.headshots_items, styles.serious_headshot].join(" ")}
 			src="/assets/headshots/serious-straight-headshot.jpg"
 			alt="Headshot Not Found"
 			/>
 
-			<img className={styles.headshots_items}
+			<img className={[styles.headshots_items, styles.seedy_headshot].join(" ")}
 			src="/assets/headshots/seedy-detective.png"
 			alt="Headshot Not Found"
 			/>
 
-			<img className={styles.headshots_items}
+			<img className={[styles.headshots_items, styles.jeanjacket_headshot].join(" ")}
 			src="/assets/headshots/jean-jacket.jpg"
 			alt="Headshot Not Found"
 			/>
 
-			<img className={styles.headshots_items}
+			<img className={[styles.headshots_items, styles.earnest_headshot].join(" ")}
 			src="/assets/headshots/earnest.jpg"
 			alt="Headshot Not Found"
 			/>
