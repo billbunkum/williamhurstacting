b2dae66d448430ad34a7e97402fdb539044808a2
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-02-14 23:07:16 -0800

    wrestle with Materialize and Flexbox and NextJS

--- pages/components/_contact-form.js
+++ pages/components/_contact-form.js
@@ -4,28 +4,40 @@ export default function ContactForm() {
 	return (
 		<>
 		<div className={styles.contact_form}>
-
 			<form
   			action="https://formspree.io/f/xvoearob"
   			method="POST"
 			>
-				<input id="fullname" placeholder="Full Name" 
-					type="text" 
-					name="fullname" />
-				<label htmlfor="fullname">Your Name</label>
+
+				<div className="input-field">
+					<input id="fullname" placeholder="Full Name" 
+						type="text" 
+						name="fullname" />
+					<label htmlfor="fullname">Your Name</label>
+				</div>
+
+				<div className="input-field">	
+					<input id="email" placeholder="you@email-address.com"
+							data-length="320"
+							type="email"
+							name="email" />
+					<label htmlfor="email">Your Email</label>
+				</div>  			
 				
-				<input id="email" placeholder="you@email-address.com"
-						type="email"
-						name="email" />
-				<label htmlfor="email">Your Email</label>
-  			
-				<textarea id="message" placeholder="Hey William!, you're perfect for the part!"
-					name="message"
-					className={styles.text_area}
-				></textarea>
-				<label htmlfor="message">Your Message</label>
+				<div className="input-field">
+					<textarea id="message" placeholder="Hey William!, you're perfect for the part!"
+						data-length="500"
+						name="message"
+						className={styles.text_area}
+					></textarea>
+					<label htmlfor="message">Your Message</label>
+				</div>
+
 				<br />	
-  			<button type="submit">Send</button>
+				<a
+					type="submit" 
+					className="purple waves-effect waves-light btn-large">Send</a>
+{/*  			<button type="submit">Send</button>*/}
 			</form>
 		</div>
 		</>
