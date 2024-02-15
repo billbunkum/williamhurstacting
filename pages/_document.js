bef7cb239839fc93e2a1909f95bc292e627046f3
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-02-14 21:13:30 -0800

    add MaterializeCSS CDN to index and _document

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+		{/*<!-- Compiled and minified JavaScript -->*/}
+			<script
+				src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </Html>
   )
 }
--- pages/components/_header-part.js
+++ pages/components/_header-part.js
@@ -13,6 +13,10 @@ export default function HeaderPart() {
     	<meta name="description" content="NextJS App" />
     	<meta name="viewport" content="width=device-width, initial-scale=1" />
     	<link rel="icon" href="/favicon.ico" />
+    {/*<!-- Compiled and minified CSS -->*/}
+    	<link 
+				rel="stylesheet" 
+				href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
 		</Head>
 		</>
 	);
