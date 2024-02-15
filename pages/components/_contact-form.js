aa2b6e54c767f518f5283639d9e36222862f3d73
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-02-14 20:40:13 -0800

    basic form styling

--- pages/components/_contact-form.js
+++ pages/components/_contact-form.js
@@ -3,21 +3,38 @@ import styles from '@/styles/Home.module.css'
 export default function ContactForm() {
 	return (
 		<>
-<form
-  action="https://formspree.io/f/xvoearob"
-  method="POST"
->
-  <label>
-    Your email:
-    <input type="email" name="email" />
-  </label>
-  <label>
-    Your message:
-    <textarea name="message"></textarea>
-  </label>
-  {/*<!-- your other form fields go here -->*/}
-  <button type="submit">Send</button>
-</form>
+			<form
+  		action="https://formspree.io/f/xvoearob"
+  		method="POST"
+			className={styles.contact_form}
+			>
+				<label for="fullname">Your Name:
+					<input 
+						id="fullname"
+						type="fullname" 
+						value="Full Name"
+						name="fullname" />
+				 </label>
+  			
+				<label for="email">Email:
+    			<input
+						id="email"
+						type="email"
+						value="Email Address"
+						name="email" />
+  			 </label>
+  			
+				<label for="message">Message:
+    			<textarea
+						id="message"
+						value="Hello William! We'd love to book you!"
+						name="message"
+						className={styles.text_area}
+					></textarea>
+  			 </label>
+{/*<!-- your other form fields go here -->*/}
+  			<button type="submit">Send</button>
+			</form>
 		</>
 	);
 }
