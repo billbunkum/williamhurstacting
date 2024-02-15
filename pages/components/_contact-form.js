720b692b49c9c82a1ba34d5daa606b2cb67f6278
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2024-02-14 22:33:43 -0800

    use flexbox and installed MaterlizeCSS to do basic style on contact-form

--- pages/components/_contact-form.js
+++ pages/components/_contact-form.js
@@ -3,38 +3,31 @@ import styles from '@/styles/Home.module.css'
 export default function ContactForm() {
 	return (
 		<>
+		<div className={styles.contact_form}>
+
 			<form
-  		action="https://formspree.io/f/xvoearob"
-  		method="POST"
-			className={styles.contact_form}
+  			action="https://formspree.io/f/xvoearob"
+  			method="POST"
 			>
-				<label for="fullname">Your Name:
-					<input 
-						id="fullname"
-						type="fullname" 
-						value="Full Name"
-						name="fullname" />
-				 </label>
-  			
-				<label for="email">Email:
-    			<input
-						id="email"
+				<input id="fullname" placeholder="Full Name" 
+					type="text" 
+					name="fullname" />
+				<label htmlfor="fullname">Your Name</label>
+				
+				<input id="email" placeholder="you@email-address.com"
 						type="email"
-						value="Email Address"
 						name="email" />
-  			 </label>
+				<label htmlfor="email">Your Email</label>
   			
-				<label for="message">Message:
-    			<textarea
-						id="message"
-						value="Hello William! We'd love to book you!"
-						name="message"
-						className={styles.text_area}
-					></textarea>
-  			 </label>
-{/*<!-- your other form fields go here -->*/}
+				<textarea id="message" placeholder="Hey William!, you're perfect for the part!"
+					name="message"
+					className={styles.text_area}
+				></textarea>
+				<label htmlfor="message">Your Message</label>
+				<br />	
   			<button type="submit">Send</button>
 			</form>
+		</div>
 		</>
 	);
 }
