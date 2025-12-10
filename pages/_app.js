bd58e154ecb1480cc9f4a375a74236dba76bd5b6
Bill Bunkum <billbunkum@Billy-Boy-Uno.lan>
2025-12-09 16:48:08 -0800

    add Head to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,16 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-		<Component {...pageProps} />
+		<>
+			 <Head>
+        <meta
+          httpEquiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
+      </Head>
+			<Component {...pageProps} />
+		</>
 	)
 }
